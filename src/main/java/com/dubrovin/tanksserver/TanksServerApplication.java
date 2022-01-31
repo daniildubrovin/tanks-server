bb0ec5b4c079c6ee80d2607278822e4cbd99d12c
dubrovin <daniildubr0vin@outlook.com>
2022-01-31 18:06:56 +0300

    Init

---
+++ src/main/java/com/dubrovin/tanksserver/TanksServerApplication.java
@@ -0,0 +1,11 @@
+package com.dubrovin.tanksserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanksServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TanksServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dubrovin/tanksserver/TanksServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dubrovin.tanksserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanksServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
